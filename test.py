539471796ce0960c928e2e539e96d3e2b1da2526
Linas <lmzedas@gmail.com>
2021-08-16 11:28:07 +0300

    Add test.py file

---
+++ test.py
@@ -0,0 +1,16 @@
+import bluetooth, sys
+
+addr = "98:D3:71:F9:CF:2E"
+
+try:
+    conn = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    conn.connect((addr,1))
+except bluetooth.btcommon.BluetoothError as err:
+    print(err)
+    pass
+
+def activatePump(pumpNo, time):
+    conn.send("|"+pumpNo+":"+time+"|")
+
+if __name__ == "__main__":
+    activatePump(sys.argv[1], sys.argv[2])
